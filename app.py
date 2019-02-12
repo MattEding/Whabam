2df64ee630481956192c064b53be246d92f18bc3
Matt Eding <matteding@gmail.com>
2019-02-11 17:26:15 -0800

    removed unused files; added db to df notebook

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ from utility import find_file, beat_synchronous_chroma, dynamic_tempo_estimation
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return '/html/index.html'
+    return 'Enter /analyze/<title> to search for a song and display its chroma and tempo graphs'
+
 
 @app.route('/analyze/<title>')
 def analyze(title):
@@ -20,7 +22,6 @@ def analyze(title):
     segments, name = npz.values()
     name = np.asscalar(name)
     seg0 = segments[0]
-    # chroma_sync, beat_time = beat_synchronous_chroma(seg0)
 
     chroma_path = api.save_plot_chroma(seg0, name)
     chroma_path = flask.url_for('static', filename=chroma_path)
